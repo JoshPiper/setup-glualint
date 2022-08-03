4497c4032f778f69bf85a9e857b24e98a63dd9c4
JoshPiper <32164094+JoshPiper@users.noreply.github.com>
2022-08-03 16:03:43 +0100

    chore: backdate to gtk2.

--- src/versions/1.1.0.ts
+++ src/versions/1.1.0.ts
@@ -12,7 +12,7 @@ export default class Version1p1p0 extends LintVersion {
             encoding: "utf-8"
         }))
 
-        console.log(execSync('sudo apt-get install -y libgtk-3-dev'))
+        console.log(execSync('sudo apt-get install -y libgtk-2-dev'))
         console.log(execSync("cabal update", {
             cwd: downloadFolder,
             encoding: "utf-8"
